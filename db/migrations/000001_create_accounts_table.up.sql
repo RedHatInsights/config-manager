6607793213b756ff1a935645766f07a0f8218841
Jason Stephens <jassteph@redhat.com>
2021-01-15 15:53:20 -0500

    Spacing fix in migrations

--- db/migrations/000001_create_accounts_table.up.sql
+++ db/migrations/000001_create_accounts_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS accounts
 (
-	account_id TEXT NOT NULL,
-	state JSONB NOT NULL,
-	CONSTRAINT accounts_pkey PRIMARY KEY (account_id)
+    account_id TEXT NOT NULL,
+    state JSONB NOT NULL,
+    CONSTRAINT accounts_pkey PRIMARY KEY (account_id)
 );
