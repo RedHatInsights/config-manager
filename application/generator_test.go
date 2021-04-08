21e785faa0e4a429ba5dda820bcc59eb1cd1466d
Jason Stephens <jassteph@redhat.com>
2021-04-08 14:51:05 -0400

    Checking existence of play before adding it to the playbook

--- application/generator_test.go
+++ application/generator_test.go
@@ -82,3 +82,42 @@ func TestGenerateAllDisabled(t *testing.T) {
 		t.Errorf("Received playbook did not contain expected plays: %s", pb)
 	}
 }
+
+func TestGenerateWhenServiceDoesNotHavePlay(t *testing.T) {
+	templates := utils.FilesIntoMap("../playbooks/test/", "*.yml")
+
+	pbg := &Generator{
+		Templates: templates,
+	}
+
+	state := domain.StateMap{
+		"test1":              "disabled",
+		"test2":              "disabled",
+		"serviceWithoutPlay": "disabled",
+	}
+
+	pb, err := pbg.GeneratePlaybook(state)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expectedPlay1 := `
+- name: Test 1
+  hosts: localhost
+  tasks:
+    - name: Print 1 removed
+      debug:
+        msg: "1 removed"`
+
+	expectedPlay2 := `
+- name: Test 2
+  hosts: localhost
+  tasks:
+    - name: Print 2 removed
+      debug:
+        msg: "2 removed"`
+
+	if !strings.Contains(pb, expectedPlay1) || !strings.Contains(pb, expectedPlay2) {
+		t.Errorf("Received playbook did not contain expected plays: %s", pb)
+	}
+}
