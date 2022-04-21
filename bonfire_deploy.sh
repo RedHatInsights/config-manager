340527a7a38e9a54261e5739849898dcc3927cc7
Link Dupont <link@sub-pop.net>
2022-04-21 11:44:47 -0700

    refactor: fix shellcheck lint warnings

--- bonfire_deploy.sh
+++ bonfire_deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-TAG=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1`
+
+TAG=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 7 | head -n 1)
 IMAGE="localhost:5000/config-manager"
-podman build -t $IMAGE:$TAG -f Dockerfile
-podman push $IMAGE:$TAG `minikube ip`:5000/config-manager:$TAG --tls-verify=false
-bonfire deploy -c ./bonfire_config.yaml --get-dependencies --namespace config-manager -p config-manager/config-manager/$IMAGE_TAG=$TAG config-manager -i $IMAGE=$TAG
-echo $TAG
+IP=$(minikube ip)
+
+podman build -t "$IMAGE:$TAG" -f Dockerfile
+podman push $IMAGE:"$TAG" "$IP":5000/config-manager:"$TAG" --tls-verify=false
+bonfire deploy -c ./bonfire_config.yaml --get-dependencies --namespace config-manager -p config-manager/config-manager/"$IMAGE_TAG"="$TAG" config-manager -i $IMAGE="$TAG"
+echo "$TAG"
