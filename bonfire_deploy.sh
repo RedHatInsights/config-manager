92ce6f6a1e6c29944462d07536088ab7253695df
Link Dupont <link@sub-pop.net>
2022-04-22 14:50:58 -0700

    refactor: clean up variable quoting
    further clean up variable quoting in bonfire_deploy.sh

--- bonfire_deploy.sh
+++ bonfire_deploy.sh
@@ -4,7 +4,7 @@ TAG=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 7 | head -n 1)
 IMAGE="localhost:5000/config-manager"
 IP=$(minikube ip)
 
-podman build -t "$IMAGE:$TAG" -f Dockerfile
-podman push $IMAGE:"$TAG" "$IP":5000/config-manager:"$TAG" --tls-verify=false
-bonfire deploy -c ./bonfire_config.yaml --get-dependencies --namespace config-manager --set-parameter config-manager/config-manager/"$IMAGE"_"$TAG"="$TAG" --set-image-tag $IMAGE="$TAG" config-manager
-echo "$TAG"
+podman build -t "${IMAGE}:${TAG}" -f Dockerfile
+podman push "${IMAGE}:${TAG}" "${IP}:5000/config-manager:${TAG}" --tls-verify=false
+bonfire deploy -c ./bonfire_config.yaml --get-dependencies --namespace config-manager --set-parameter "config-manager/config-manager/IMAGE_TAG=${TAG}" --set-image-tag "${IMAGE}=${TAG}" config-manager
+echo "${TAG}"
