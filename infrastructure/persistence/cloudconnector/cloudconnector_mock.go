78e0f575e1cfae95ebc7ff75650d1ca6cebc0ea9
Link Dupont <link@sub-pop.net>
2022-04-27 13:42:57 -0400

    feat: add support GetConnectionStatus method
    Expand the CloudConnectorClient type to get the connection status
    details of a node.

--- infrastructure/persistence/cloudconnector/cloudconnector_mock.go
+++ infrastructure/persistence/cloudconnector/cloudconnector_mock.go
@@ -15,3 +15,9 @@ func (c *cloudConnectorClientMock) GetConnections(ctx context.Context, accountID
 func (c *cloudConnectorClientMock) SendMessage(ctx context.Context, accountID string, directive string, payload []byte, metadata map[string]string, recipient string) (string, error) {
 	return "0afbfb55-a2af-43f2-84da-a0896f03f067", nil
 }
+
+func (c *cloudConnectorClientMock) GetConnectionStatus(ctx context.Context, accountID string, recipient string) (string, map[string]interface{}, error) {
+	return "connected", map[string]interface{}{
+		"rhc-worker-playbook": map[string]interface{}{},
+	}, nil
+}
