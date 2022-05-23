44a345a67096a456c7b0fc9715c34419e1c0c002
Link Dupont <link@sub-pop.net>
2022-05-23 15:03:01 -0400

    refactor: make changes to bonfire deployment
    Update the config file parameters to include just the local parameters
    necessary (maybe?) to deploy with bonfire exclusively. Additionally,
    rewrite the bonfire deploy script to use a traceable value for tags and
    set the runtime parameter overrides based on the generated tag.

--- bonfire_deploy.sh
+++ bonfire_deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-TAG=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 7 | head -n 1)
-IMAGE="localhost:5000/config-manager"
+set -xe
+
+HEAD_UNIX=$(date --date="$(git show --no-patch --format='%cI' HEAD)" +%s)
+NOW_UNIX=$(date +%s)
+SECDIFF=$(("${NOW_UNIX}" - "${HEAD_UNIX}"))
+TAG="$(git rev-parse --short HEAD)-${SECDIFF}"
+IMAGE="config-manager"
 IP=$(minikube ip)
 
 podman build -t "${IMAGE}:${TAG}" -f Dockerfile
-podman push "${IMAGE}:${TAG}" "${IP}:5000/config-manager:${TAG}" --tls-verify=false
-bonfire deploy -c ./bonfire_config.yaml --get-dependencies --namespace config-manager --set-parameter "config-manager/config-manager/IMAGE_TAG=${TAG}" --set-image-tag "${IMAGE}=${TAG}" config-manager
-echo "${TAG}"
+podman push "${IMAGE}:${TAG}" "${IP}:5000/${IMAGE}:${TAG}" --tls-verify=false
+bonfire deploy \
+    --local-config-path ./bonfire_config.yaml \
+    --get-dependencies \
+    --namespace config-manager \
+    --set-parameter "config-manager/CM_PLAYBOOK_HOST=http://${IP}/" \
+    --set-parameter "config-manager/IMAGE=localhost:5000/${IMAGE}" \
+    --set-parameter "config-manager/IMAGE_TAG=${TAG}" \
+    --set-image-tag "quay.io/cloudservices/config-manager=${TAG}" \
+    config-manager
