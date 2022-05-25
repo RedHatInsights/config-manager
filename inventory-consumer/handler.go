93c30f0a0efeb6a63882c1c8cec6b3830bf431ae
Link Dupont <link@sub-pop.net>
2022-05-25 14:23:09 -0400

    refactor(inventory-consumer): improve logging
    Improve the logging output for the inventory consumer message handler
    function.

--- inventory-consumer/handler.go
+++ inventory-consumer/handler.go
@@ -26,59 +26,61 @@ type requestIDkey string
 // loop. It unmarshals the received message and evaluates whether state should
 // be applied for the identified host, applying as needed.
 func (this *handler) onMessage(ctx context.Context, msg kafka.Message) {
+	log.Trace().Str("module", "inventory-consumer").Msg("received message")
 	eventType, err := kafkaUtils.GetHeader(msg, "event_type")
 	if err != nil {
 		log.Error().Err(err).Msgf("Error getting event_type: ", err)
 		return
 	}
+	log.Trace().Str("module", "inventory-consumer").Msgf("event_type = %v", eventType)
 
 	if eventType == "created" || eventType == "updated" {
 		value := &message.InventoryEvent{}
 
 		if err := json.Unmarshal(msg.Value, &value); err != nil {
-			log.Error().Err(err).Msgf("Couldn't unmarshal inventory event: ", err)
+			log.Error().Str("module", "inventory-consumer").Err(err).Msg("couldn't unmarshal inventory event")
 			return
 		}
 
 		if value.Host.Reporter == "cloud-connector" {
 			if eventType == "created" {
-				log.Info().Msgf("New host detected; setting up for playbook execution")
+				log.Info().Str("module", "inventory-consumer").Msg("new host detected; setting up for playbook execution")
 				messageID, err := this.ConfigManagerService.SetupHost(ctx, value.Host)
 				if err != nil {
-					log.Info().Msgf("Error setting up host: %v: %v", value.Host, err)
+					log.Error().Str("module", "inventory-consumer").Err(err).Msgf("error setting up host: %v", value.Host)
 					return
 				}
-				log.Info().Msgf("Cloud-connector setup host message id: %v", messageID)
+				log.Info().Str("module", "inventory-consumer").Msgf("Cloud-connector setup host message id: %v", messageID)
 			}
 
 			accState, err := this.ConfigManagerService.GetAccountState(value.Host.Account)
 			if err != nil {
-				log.Info().Msgf("Error retrieving state for account: %v: %v", value.Host.Account, err)
+				log.Error().Str("module", "inventory-consumer").Err(err).Msgf("Error retrieving state for account: %v", value.Host.Account)
 				return
 			}
 
 			reqID, err := kafkaUtils.GetHeader(msg, "request_id")
 			if err != nil {
-				log.Error().Err(err).Msgf("Error getting request_id: ", err)
+				log.Error().Str("module", "inventory-consumer").Err(err).Msg("Error getting request_id header")
 				k := requestIDkey("request_id")
 				reqID = uuid.New().String()
-				log.Info().Msgf("Creating new request_id and adding to context: ", reqID)
+				log.Info().Str("module", "inventory-consumer").Msgf("Creating new request_id and adding to context: %v", reqID)
 				ctx = context.WithValue(ctx, k, reqID)
 			}
 
-			log.Info().Msgf("Cloud-connector inventory event request_id: %s, data: %+v", reqID, value)
+			log.Info().Str("module", "inventory-consumer").Msgf("Cloud-connector inventory event request_id: %s, data: %+v", reqID, value)
 
 			if value.Host.SystemProfile.RHCState != accState.StateID.String() {
-				log.Info().Msgf("rhc_state_id %s for client %s does not match current state id %s for account %s. Updating.",
+				log.Info().Str("module", "inventory-consumer").Msgf("rhc_state_id %s for client %s does not match current state id %s for account %s. Updating.",
 					value.Host.SystemProfile.RHCState, value.Host.SystemProfile.RHCID, accState.StateID.String(), accState.AccountID)
 				client := []domain.Host{value.Host}
 				responses, err := this.ConfigManagerService.ApplyState(ctx, accState, client)
 				if err != nil {
-					log.Error().Err(err).Msgf("Error applying state: ", err)
+					log.Error().Str("module", "inventory-consumer").Err(err).Msg("error applying state")
 				}
-				log.Info().Msgf("Message sent to the dispatcher. Results: ", responses)
+				log.Info().Str("module", "inventory-consumer").Msgf("Message sent to the dispatcher. Results: %v", responses)
 			} else {
-				log.Info().Msgf("rhc_state_id %s for client %s is up to date for account %s. Not updating.",
+				log.Info().Str("module", "inventory-consumer").Msgf("rhc_state_id %s for client %s is up to date for account %s. Not updating.",
 					value.Host.SystemProfile.RHCState, value.Host.SystemProfile.RHCID, accState.AccountID)
 			}
 		}
