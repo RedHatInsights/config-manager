631405bff6ab6fc08bd638b9672b52db96381485
Link Dupont <link@sub-pop.net>
2022-05-27 14:04:39 -0400

    build: dynamically identify the namespace
    When deploying config-manager, dynamically identify the targeted
    namespace by examining the ClowdEnvironment. Move all dynamic parameters
    into the shell script, keeping static parameters in the config file.

--- bonfire_deploy.sh
+++ bonfire_deploy.sh
@@ -8,15 +8,19 @@ SECDIFF=$(("${NOW_UNIX}" - "${HEAD_UNIX}"))
 TAG="$(git rev-parse --short HEAD)-${SECDIFF}"
 IMAGE="config-manager"
 IP=$(minikube ip)
+TARGET_NAMESPACE=$(minikube kubectl -- get env -o json | jq '.items[0].spec.targetNamespace' -r)
 
 podman build -t "${IMAGE}:${TAG}" -f Dockerfile
 podman push "${IMAGE}:${TAG}" "${IP}:5000/${IMAGE}:${TAG}" --tls-verify=false
 bonfire deploy \
     --local-config-path ./bonfire_config.yaml \
     --get-dependencies \
-    --namespace config-manager \
+    --namespace "${TARGET_NAMESPACE}" \
     --set-parameter "config-manager/CM_PLAYBOOK_HOST=http://${IP}/" \
     --set-parameter "config-manager/IMAGE=localhost:5000/${IMAGE}" \
     --set-parameter "config-manager/IMAGE_TAG=${TAG}" \
+    --set-parameter "config-manager/CM_DISPATCHER_HOST=http://playbook-dispatcher-api.${TARGET_NAMESPACE}.svc.cluster.local:8000/" \
+    --set-parameter "config-manager/CM_INVENTORY_HOST=http://host-inventory-service.${TARGET_NAMESPACE}.svc.cluster.local:8000/" \
+    --set-parameter "config-manager/CM_CLOUD_CONNECTOR_HOST=http://cloud-connector.${TARGET_NAMESPACE}.svc.cluster.local:8080/api/cloud-connector/v1/" \
     --set-image-tag "quay.io/cloudservices/config-manager=${TAG}" \
     config-manager
