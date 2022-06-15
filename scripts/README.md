67cc9936ccfd82c6fc64d8c63c843ec1857def16
Link Dupont <link@sub-pop.net>
2022-06-15 14:53:59 -0400

    chore: clean up developer scripts and tooling
    Delete unnecessary files such as docker-compose.yml, cdappconfig.json
    and Makefile. Move the local bonfire deployment script to scripts/.

--- scripts/README.md
+++ scripts/README.md
@@ -7,3 +7,10 @@ This directory contains scripts useful to developing config-manager.
 `kube-port-forward.sh` is a simple process management script around the `kubectl
 port-forward` command. It allows all the ports necessary for local development
 to be forwarded to the needed services.
+
+## `bonfire-deploy.sh`
+
+`bonfire-deploy.sh` builds a container image using `Dockerfile`, pushes it to a
+register that is assumed to exist inside a minikube cluster, and then deploys it
+using bonfire, overriding image tags and references using a combination of
+`bonfire.yml` and command-line parameters.
--- scripts/bonfire-deploy.sh
+++ scripts/bonfire-deploy.sh
@@ -14,14 +14,11 @@ HOST_IP=$(nmcli --terse connection show virbr0 | grep ipv4.addresses | cut -d":"
 podman build -t "${IMAGE}:${TAG}" -f Dockerfile
 podman push "${IMAGE}:${TAG}" "${CLUSTER_IP}:5000/${IMAGE}:${TAG}" --tls-verify=false
 bonfire deploy \
-    --local-config-path ./bonfire_config.yaml \
+    --local-config-path "$(dirname ${0})/bonfire.yml" \
     --get-dependencies \
     --namespace "${TARGET_NAMESPACE}" \
     --set-parameter "config-manager/CM_PLAYBOOK_HOST=http://${HOST_IP}:8080" \
     --set-parameter "config-manager/IMAGE=localhost:5000/${IMAGE}" \
     --set-parameter "config-manager/IMAGE_TAG=${TAG}" \
-    --set-parameter "config-manager/CM_DISPATCHER_HOST=http://playbook-dispatcher-api.${TARGET_NAMESPACE}.svc.cluster.local:8000/" \
-    --set-parameter "config-manager/CM_INVENTORY_HOST=http://host-inventory-service.${TARGET_NAMESPACE}.svc.cluster.local:8000/" \
-    --set-parameter "config-manager/CM_CLOUD_CONNECTOR_HOST=http://cloud-connector.${TARGET_NAMESPACE}.svc.cluster.local:8080/api/cloud-connector/v1/" \
     --set-image-tag "quay.io/cloudservices/config-manager=${TAG}" \
     config-manager
