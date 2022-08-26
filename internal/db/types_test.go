d2e1634cec42be839d3e783dc2425f4358be34f3
Link Dupont <link@sub-pop.net>
2022-08-26 13:47:26 -0400

    feat: add JSONNullString type

--- internal/db/types_test.go
+++ internal/db/types_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestJSONNullBoolMarshalJSON(t *testing.T) {
@@ -136,6 +137,137 @@ func TestJSONNullBoolUnmarshalJSON(t *testing.T) {
 		t.Run(test.description, func(t *testing.T) {
 			var got JSONNullBool
 
+			err := got.UnmarshalJSON(test.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !cmp.Equal(got, test.want) {
+				t.Errorf("%#v != %#v", got, test.want)
+			}
+		})
+	}
+}
+
+func TestJSONNullStringMarshalJSON(t *testing.T) {
+	tests := []struct {
+		description string
+		input       JSONNullString
+		want        []byte
+		wantError   error
+	}{
+		{
+			description: "valid, non-empty string",
+			input: JSONNullString{
+				NullString: sql.NullString{
+					Valid:  true,
+					String: `abcd`,
+				},
+			},
+			want: []byte(`"abcd"`),
+		},
+		{
+			description: "valid, empty string",
+			input: JSONNullString{
+				NullString: sql.NullString{
+					Valid:  true,
+					String: ``,
+				},
+			},
+			want: []byte(`""`),
+		},
+		{
+			description: "invalid, non-empty string",
+			input: JSONNullString{
+				NullString: sql.NullString{
+					Valid:  false,
+					String: `"abcd"`,
+				},
+			},
+			want: []byte(`""`),
+		},
+		{
+			description: "invalid, empty string",
+			input: JSONNullString{
+				NullString: sql.NullString{
+					Valid:  false,
+					String: `""`,
+				},
+			},
+			want: []byte(`""`),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			got, err := test.input.MarshalJSON()
+
+			if test.wantError != nil {
+				if !cmp.Equal(err, test.wantError, cmpopts.EquateErrors()) {
+					t.Errorf("%#v != %#v", err, test.wantError)
+				}
+			} else {
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if !json.Valid(got) {
+					t.Errorf("got invalid JSON: %#v", string(got))
+				}
+
+				if !cmp.Equal(got, test.want) {
+					t.Errorf("%v != %v", got, test.want)
+				}
+			}
+		})
+	}
+}
+
+func TestJSONNullStringUnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		description string
+		input       []byte
+		want        JSONNullString
+	}{
+		{
+			description: "non-empty string input",
+			input:       []byte(`"abcd"`),
+			want: JSONNullString{
+				NullString: sql.NullString{
+					Valid:  true,
+					String: "abcd",
+				},
+			},
+		},
+		{
+			description: "empty string input",
+			input:       []byte(`""`),
+			want: JSONNullString{
+				NullString: sql.NullString{
+					Valid:  true,
+					String: "",
+				},
+			},
+		},
+		{
+			description: "null input",
+			input:       []byte(`null`),
+			want: JSONNullString{
+				NullString: sql.NullString{
+					Valid:  false,
+					String: "",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			if !json.Valid(test.input) {
+				t.Errorf("got invalid JSON: %#v", string(test.input))
+			}
+
+			var got JSONNullString
 			err := got.UnmarshalJSON(test.input)
 			if err != nil {
 				t.Fatal(err)
