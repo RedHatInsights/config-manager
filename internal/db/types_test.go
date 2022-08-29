93eaeac853e10001cb834b74f2e2012a5bac0d22
Link Dupont <link@sub-pop.net>
2022-08-29 11:41:15 -0400

    test: add json (un)marshal tests for Profile struct

--- internal/db/types_test.go
+++ internal/db/types_test.go
@@ -4,10 +4,103 @@ import (
 	"database/sql"
 	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/google/uuid"
 )
+
+func TestProfileMarshalJSON(t *testing.T) {
+	tests := []struct {
+		description string
+		input       Profile
+		want        []byte
+		wantError   error
+	}{
+		{
+			input: Profile{
+				ID:           uuid.MustParse("a863569d-6e57-4082-ba80-20e0089738ff"),
+				Name:         &JSONNullString{NullString: sql.NullString{Valid: true, String: "default"}},
+				Label:        &JSONNullString{NullString: sql.NullString{Valid: true, String: "91fb2f8b"}},
+				AccountID:    &JSONNullString{NullString: sql.NullString{Valid: true, String: "123456"}},
+				OrgID:        &JSONNullString{NullString: sql.NullString{Valid: true, String: "654321"}},
+				CreatedAt:    time.Unix(0, 0).UTC(),
+				Active:       true,
+				Creator:      &JSONNullString{NullString: sql.NullString{Valid: true, String: "root"}},
+				Insights:     true,
+				Remediations: true,
+				Compliance:   true,
+			},
+			want: []byte(`{"profile_id":"a863569d-6e57-4082-ba80-20e0089738ff","name":"default","label":"91fb2f8b","account_id":"123456","org_id":"654321","created_at":"1970-01-01T00:00:00Z","active":true,"creator":"root","insights":true,"remediations":true,"compliance":true}`),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			got, err := json.Marshal(test.input)
+
+			if test.wantError != nil {
+				if !cmp.Equal(err, test.wantError, cmpopts.EquateErrors()) {
+					t.Errorf("%#v != %#v", err, test.wantError)
+				}
+			} else {
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !cmp.Equal(got, test.want) {
+					t.Errorf("%v", cmp.Diff(got, test.want))
+				}
+			}
+		})
+	}
+}
+
+func TestProfileUnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		description string
+		input       []byte
+		want        Profile
+		wantError   error
+	}{
+		{
+			input: []byte(`{"profile_id":"a863569d-6e57-4082-ba80-20e0089738ff","name":"default","label":"91fb2f8b","account_id":"123456","org_id":"654321","created_at":"1970-01-01T00:00:00Z","active":true,"creator":"root","insights":true,"remediations":true,"compliance":true}`),
+			want: Profile{
+				ID:           uuid.MustParse("a863569d-6e57-4082-ba80-20e0089738ff"),
+				Name:         &JSONNullString{NullString: sql.NullString{Valid: true, String: "default"}},
+				Label:        &JSONNullString{NullString: sql.NullString{Valid: true, String: "91fb2f8b"}},
+				AccountID:    &JSONNullString{NullString: sql.NullString{Valid: true, String: "123456"}},
+				OrgID:        &JSONNullString{NullString: sql.NullString{Valid: true, String: "654321"}},
+				CreatedAt:    time.Unix(0, 0).UTC(),
+				Active:       true,
+				Creator:      &JSONNullString{NullString: sql.NullString{Valid: true, String: "root"}},
+				Insights:     true,
+				Remediations: true,
+				Compliance:   true,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			var got Profile
+			err := json.Unmarshal(test.input, &got)
+
+			if test.wantError != nil {
+				if !cmp.Equal(err, test.wantError, cmpopts.EquateErrors()) {
+					t.Errorf("%#v != %#v", err, test.wantError)
+				}
+			} else {
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !cmp.Equal(got, test.want) {
+					t.Errorf("%v", cmp.Diff(got, test.want))
+				}
+			}
+		})
+	}
+}
 
 func TestJSONNullBoolMarshalJSON(t *testing.T) {
 	tests := []struct {
