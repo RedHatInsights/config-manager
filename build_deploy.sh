1d55e410f2f5bb15cb9240d03c720db2ea9940e3
Rohini Chandra <61837065+r14chandra@users.noreply.github.com>
2024-04-11 15:00:05 +0530

    RHINENG-9414 use tmp dir for docker config (#159)

--- build_deploy.sh
+++ build_deploy.sh
@@ -10,12 +10,24 @@ if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
     exit 1
 fi
 
-DOCKER_CONF="$PWD/.docker"    
+# Create tmp dir to store data in during job run (do NOT store in $WORKSPACE)
+export TMP_JOB_DIR=$(mktemp -d -p "$HOME" -t "jenkins-${JOB_NAME}-${BUILD_NUMBER}-XXXXXX")
+echo "job tmp dir location: $TMP_JOB_DIR"
+
+function job_cleanup() {
+    echo "cleaning up job tmp dir: $TMP_JOB_DIR"
+    rm -fr $TMP_JOB_DIR
+}
+
+trap job_cleanup EXIT ERR SIGINT SIGTERM
+
+DOCKER_CONF="$TMP_JOB_DIR/.docker"   
+
 mkdir -p "$DOCKER_CONF"    
     
-docker login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io    
-docker build -t "${IMAGE}:${IMAGE_TAG}" .    
-docker tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"    
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io    
+docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" .    
+docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"    
     
-docker push "${IMAGE}:${IMAGE_TAG}"    
-docker push "${IMAGE}:latest"
+docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"    
+docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
